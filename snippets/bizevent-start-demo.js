8b8d213c85f083e332df1e825a91163ec398c968
Adam Gardner <adam.gardner@dynatrace.com>
2025-06-12 12:00:10 +1000

    Deployed c4e682b with MkDocs version: 1.6.1

--- snippets/bizevent-start-demo.js
+++ snippets/bizevent-start-demo.js
@@ -1,5 +1,5 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "8. cleanup"});
+  dynatrace.sendBizEvent('page_load', {"page": "3. start-demo"});
 });
 </script>

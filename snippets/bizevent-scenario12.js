baea9b2b0208b0a40951fd93439b49c27ee14369
Adam Gardner <adam.gardner@dynatrace.com>
2025-12-05 16:44:03 +1000

    Deployed dfe8bb8 with MkDocs version: 1.6.1

---
+++ snippets/bizevent-scenario12.js
@@ -0,0 +1,7 @@
+<script>
+var CURRENT_PAGE = "scenario-12";
+
+document.addEventListener('DOMContentLoaded', function() {
+  dynatrace.sendBizEvent('page_load', {"page": CURRENT_PAGE});
+});
+</script>
